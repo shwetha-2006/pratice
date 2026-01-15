04b55d799484a056e0d93e66bc3f6effc07fff5e
shwetha-2006 <sujatasujata45539@gmail.com>
2026-01-15 16:24:09 +0530

    pratice.py

--- pratice.py
+++ pratice.py
@@ -1,9 +1,25 @@
 import sys
 
-if len(sys.argv) != 2:
-    print("Usage: python pratice.py <celsius>")
-    sys.exit(1)
+def calculate_cube(num):
+    """Function to calculate cube of a number"""
+    return num ** 3
 
-celsius = float(sys.argv[1])
-fahrenheit = (celsius * 9/5) + 32
-print("Temperature in Fahrenheit:", fahrenheit)
+
+if __name__ == "__main__":
+    print("\n---- Cube of a Number ----\n")
+    try:
+        # Case 1: Argument passed (for Jenkins or CLI)
+        if len(sys.argv) == 2:
+            num = float(sys.argv[1])
+
+        # Case 2: No argument passed (for console use)
+        else:
+            num = float(input("Enter a number: "))
+
+        cube = calculate_cube(num)
+
+        print(f"\nNumber: {num}")
+        print(f"Cube = {cube}\n")
+
+    except ValueError:
+        print("Invalid input! Please enter numeric values.")
