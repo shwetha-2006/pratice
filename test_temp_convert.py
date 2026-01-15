f4e08ac58d3a11ca9e0f35ac4b234e4261f170e7
shwetha-2006 <sujatasujata45539@gmail.com>
2026-01-16 02:57:14 +0530

    test_temp_convert.py

--- test_temp_convert.py
+++ test_temp_convert.py
@@ -1,13 +1,5 @@
-from cube_number import calculate_cube
+from pratice import is_palindrome
 
-def test_calculate_cube_positive():
-    assert calculate_cube(3) == 27
-
-def test_calculate_cube_zero():
-    assert calculate_cube(0) == 0
-
-def test_calculate_cube_negative():
-    assert calculate_cube(-2) == -8
-
-def test_calculate_cube_float():
-    assert calculate_cube(2.5) == 15.625
+def test_palindrome():
+    assert is_palindrome("madam")
+    assert not is_palindrome("hello")
