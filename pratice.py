854d39e119b7893fcc431094a095d99a5b514c80
shwetha-2006 <sujatasujata45539@gmail.com>
2026-01-15 14:16:46 +0530

    pratice.py

--- pratice.py
+++ pratice.py
@@ -1,9 +1,9 @@
 import sys
 
-def celsius_to_fahrenheit(celsius):
-    return (celsius * 9/5) + 32
+if len(sys.argv) != 2:
+    print("Usage: python pratice.py <celsius>")
+    sys.exit(1)
 
-if __name__ == "__main__":
-    celsius = float(sys.argv[1])
-    result = celsius_to_fahrenheit(celsius)
-    print("Temperature in Fahrenheit:", result)
+celsius = float(sys.argv[1])
+fahrenheit = (celsius * 9/5) + 32
+print("Temperature in Fahrenheit:", fahrenheit)
