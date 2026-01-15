38436de06710d7bb2f552af86979f230f0072f3f
shwetha-2006 <sujatasujata45539@gmail.com>
2026-01-15 13:45:43 +0530

    pratice.py

--- pratice.py
+++ pratice.py
@@ -1,3 +1,9 @@
-celsius = float(input("Enter temperature in Celsius: "))
-fahrenheit = (celsius * 9/5) + 32
-print("Temperature in Fahrenheit:", fahrenheit)
+import sys
+
+def celsius_to_fahrenheit(celsius):
+    return (celsius * 9/5) + 32
+
+if __name__ == "__main__":
+    celsius = float(sys.argv[1])
+    result = celsius_to_fahrenheit(celsius)
+    print("Temperature in Fahrenheit:", result)
