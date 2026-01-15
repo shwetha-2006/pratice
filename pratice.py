96da63938d4d6c001c688ccbb44318ce867ea696
shwetha-2006 <sujatasujata45539@gmail.com>
2026-01-16 02:56:31 +0530

    pratice.py

--- pratice.py
+++ pratice.py
@@ -1,25 +1,9 @@
-import sys
-
-def calculate_cube(num):
-    """Function to calculate cube of a number"""
-    return num ** 3
-
+def is_palindrome(text):
+    return text == text[::-1]
 
 if __name__ == "__main__":
-    print("\n---- Cube of a Number ----\n")
-    try:
-        # Case 1: Argument passed (for Jenkins or CLI)
-        if len(sys.argv) == 2:
-            num = float(sys.argv[1])
-
-        # Case 2: No argument passed (for console use)
-        else:
-            num = float(input("Enter a number: "))
-
-        cube = calculate_cube(num)
-
-        print(f"\nNumber: {num}")
-        print(f"Cube = {cube}\n")
-
-    except ValueError:
-        print("Invalid input! Please enter numeric values.")
+    import sys
+    if len(sys.argv) != 2:
+        print("Usage: python palindrome.py <text>")
+        sys.exit(1)
+    print("Palindrome" if is_palindrome(sys.argv[1]) else "Not Palindrome")
